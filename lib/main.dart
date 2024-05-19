d295900df7159a8b3b377345480cda8edc37115b
Adem Han <adem.han@outlook.com>
2024-05-19 11:20:50 +0300

    Login ekranı eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,58 @@
 import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -123,3 +177,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
+
+*/
